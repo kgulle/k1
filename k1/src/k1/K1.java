3bf7065457e52fdeedad7515d3d121c1b7495fe4
kgulle <kenangulle@hotmail.com>
2015-12-05 16:13:53 +0200

    kkkk

--- k1/src/k1/K1.java
+++ k1/src/k1/K1.java
@@ -5,6 +5,8 @@
  */
 package k1;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author kenan
@@ -16,6 +18,7 @@ public class K1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        ///kean,
     }
     
 }
